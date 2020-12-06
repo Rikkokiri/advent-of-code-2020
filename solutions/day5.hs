b2838b55b2a3ba840b30ef5218ec0b52cae38908
Ossian Rajala <ossian.rajala@gmail.com>
2020-12-06 18:30:58 +0200

    Day 5 completed

--- solutions/day5.hs
+++ solutions/day5.hs
@@ -1,12 +1,11 @@
 import System.IO
+import Data.List
 
 -- Day 5: Binary Boarding
 -- What is the highest seat ID on a boarding pass?
 
 seatIds :: [String] -> [Int]
 seatIds ls = map (\x -> seatHelper x 0 127) ls
-
-missingId :: [Int]
 
 seatHelper :: [Char] -> Int -> Int -> Int
 seatHelper [] ll ul = ul
@@ -19,11 +18,22 @@ column [] ll ul = ul
 column ('R':xs) ll ul = column xs ((div (ul + ll) 2) + 1) ul
 column ('L':xs) ll ul = column xs ll (div (ul + ll) 2)
 
+-- Part 2: What is the ID of your seat?
+missingId :: [Int] -> Int
+missingId [] = 0
+missingId (x:y:xs) = if abs(x-y) == 1
+                     then missingId (y:xs)
+                     else div (x+y) 2
+
+
+-- Read file contents
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile
-
 
 main ::  IO()
 main = do
   filecontent <- readLines "day5.txt"
-  print (maximum (maxId filecontent))
+  print (maximum (seatIds filecontent))
+  let sortedIds = sort (seatIds filecontent)
+  print (missingId sortedIds)
+
